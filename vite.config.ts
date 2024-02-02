82d0829279b7458dbb64114a73d9cddd815a9dee
Shad Sharma <shadanan@gmail.com>
2024-02-01 16:51:39 -0800

    Working Demo (#1)
    - Make a working Demo
    - Add running local instructions
    - Introduce Vite build configuration
    - Introduce Rust build configuration

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
+import wasm from "vite-plugin-wasm";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte(), wasm(), topLevelAwait()],
+});
